bd953c597faa88009dec34a11c6b90f7b1f8fdc0
Andrew Hilobok <hilobok@gmail.com>
2013-10-07 11:33:57 +0300

    Added trait

---
+++ lib/TaggableTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Anh\DoctrineExtensions\Taggable;
+
+/**
+ * This is a taggable trait that classes unable to extend AbstractTaggable
+ * (because they extend another class, etc) can include.
+ */
+trait AbstractTaggable
+{
+
+}
