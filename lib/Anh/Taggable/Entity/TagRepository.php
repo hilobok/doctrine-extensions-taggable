1168cff78faf39f81689cb503314f0f15c96f26b
Andrew Hilobok <hilobok@gmail.com>
2013-10-17 11:10:00 +0300

    Replaced getAllTagNames() with more universal search() in TagRepository

--- lib/Anh/Taggable/Entity/TagRepository.php
+++ lib/Anh/Taggable/Entity/TagRepository.php
@@ -7,20 +7,45 @@ use Doctrine\ORM\EntityRepository;
 class TagRepository extends EntityRepository
 {
     /**
-     * Returns array with all tag names.
+     * Search tags by given query.
+     *
+     * @param string  $query Search query
+     * @param boolean $exact Search exact query if true
+     * @param boolean $field Return this field instead of whole entity
+     * @param integer $limit Limit result set
      *
      * @return array
      */
-    public function getAllTagNames()
+    public function search($query, $exact = false, $field = null, $limit = null)
     {
-        $tags = $this->findAll();
-
-        $names = array();
-
-        foreach ($tags as $tag) {
-            $names[] = $tag->getName();
+        if (!$exact and strpos($query, '%') === false) {
+            $query = sprintf('%%%s%%', $query);
         }
 
-        return $names;
+        $query = $this->createQueryBuilder('tag')
+            ->where('tag.name like :query')
+            ->setParameter('query', $query)
+        ;
+
+        if ($field) {
+            $query->select(sprintf('tag.%s', $field));
+        }
+
+        if ($limit) {
+            $query->setMaxResults($limit);
+        }
+
+        $result = $query->getQuery()->getResult();
+
+        if ($field) {
+            $rows = $result;
+            $result = array();
+
+            foreach ($rows as $row) {
+                $result[] = $row[$field];
+            }
+        }
+
+        return $result;
     }
 }
--- tests/Anh/Taggable/Entity/TagRepositoryTest.php
+++ tests/Anh/Taggable/Entity/TagRepositoryTest.php
@@ -4,10 +4,28 @@ use Anh\Taggable\Tool\BaseTestCase;
 
 class TagRepositoryTest extends BaseTestCase
 {
-    public function testGetAllTagNames()
+    public function testSearch()
     {
         $this->loadFixture();
-        $names = $this->manager->getTagRepository()->getAllTagNames();
-        $this->assertEmpty(array_diff($names, array('pulsar', 'nebula', 'galaxy')));
+        $repository = $this->manager->getTagRepository();
+
+        $tags = $repository->search('');
+        $this->assertEquals(3, count($tags));
+
+        $tags = $repository->search('ul');
+        $this->assertEquals(2, count($tags)); // 'pULsar', 'nebULa'
+
+        $this->manager->loadOrCreateTag('puls');
+        $this->em->flush();
+        $tags = $repository->search('puls', true);
+        $this->assertEquals(1, count($tags));
+
+        $tags = $repository->search('galaxy', true, 'name');
+        $this->assertEquals(1, count($tags));
+        $this->assertEquals('galaxy', $tags[0]);
+
+        $tags = $repository->search('', false, null, 2);
+        $this->assertEquals(2, count($tags));
+        $this->assertInstanceOf('\Anh\Taggable\Entity\Tag', $tags[0]);
     }
 }
